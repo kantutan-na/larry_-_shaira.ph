035e6ff3f2c94ed4d8b93e74554cd0875bbcc335
kantutan-na <drizzith.aiart@gmail.com>
2024-11-05 22:59:46 +0800

    Add files via upload

---
+++ assets/js/jvectormap.js
@@ -0,0 +1,67 @@
+!function($) {
+    "use strict";
+    var salesData = {
+	  "Africa": 0,
+	  "AL": 0,
+	  "DZ": 0,
+	  "US": 0,
+	  "RU": 0,
+	  "IN": 0,
+	  "UK": 0,
+	  'CN': 0,
+	  'UAE': 0,
+	  'SA': 0,
+	  'PH': 3
+	};
+    $('#sales_db_world_map').vectorMap({
+    map: 'world_mill',
+    series: {
+      regions: [{
+        values: salesData,
+        scale: ['#ECECEC', '#ECECEC'],
+        normalizeFunction: 'polynomial',
+      }]
+    },
+    onRegionTipShow: function(e, el, code){
+      // el.html(el.html()+' ('+salesData[code]+' Sales)');
+    if(salesData[code] === undefined) {
+	    el.html('<h6>'+ el.html() + '</h6><p>'+'No Branch Here</p>').css({"font-size":"15px", "color":"#5B6670"});
+	}else{
+	   //label.html( '<b>'+label.html()+' - '+country.en+'</b>');
+       el.html('<h6>'+ el.html() + '</h6><p>'+salesData[code]+' Sales</p>').css({"font-size":"15px", "color":"#5B6670"});
+       //el.html('<div id="sales-map-popop">'+ el.html() + '</h6><p id="popop" class="text-center">'+salesData[code]+' Sales</p></div>').css({"font-size":"15px", "color":"#5B6670"});
+	}
+    },
+    hoverOpacity : 0.7,
+	hoverColor : true,
+	regionStyle : {
+		initial : {
+			fill : '#ECECEC'
+		},
+		hover: {
+			"fill-opacity": 1,
+            "fill": '#FF9F43',
+            "stroke": '#FF9F43'
+        },
+		selected: {
+	        fill: '#F4A582'
+	    },
+	},
+    backgroundColor : 'transparent'
+  });
+	
+	// $('#world_map').vectorMap({
+	// 	map: 'world_mill',
+	// 	scaleColors : ['#03a9f4', '#03a9f4'],
+	// 	normalizeFunction : 'polynomial',
+	// 	hoverOpacity : 0.7,
+	// 	hoverColor : false,
+	// 	regionStyle : {
+	// 		initial : {
+	// 			fill : '#7638ff'
+	// 		}
+	// 	},
+	// 	backgroundColor : 'transparent'
+	// });
+	
+}(window.jQuery)
